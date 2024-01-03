572a62750a79a490c91d63858b726428174a1ba2
Phodal Huang <h@phodal.com>
2024-01-03 10:06:32 +0800

    chore: add for new requirements

--- src/main/java/cc/unitmesh/untitled/demo/controller/BlogCategoryController.java
+++ src/main/java/cc/unitmesh/untitled/demo/controller/BlogCategoryController.java
@@ -4,6 +4,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class BlogCategoryController {
-    // devti://story/github/1
+    // devti://story/github/10
     // devti://story/github/5
 }
