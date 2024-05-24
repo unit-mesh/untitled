d4c877a8b0e628bdf5828f713fb227d119e0996b
Phodal Huang <h@phodal.com>
2024-05-24 14:02:24 +0800

    refactor(blog): rename User entity to Author, update references
    - Refactored the User entity to Author in CreateBlogRequest and related files.

--- src/main/java/cc/unitmesh/untitled/demo/dto/CreateBlogRequest.java
+++ src/main/java/cc/unitmesh/untitled/demo/dto/CreateBlogRequest.java
@@ -1,11 +1,11 @@
 package cc.unitmesh.untitled.demo.dto;
 
-import cc.unitmesh.untitled.demo.entity.User;
+import cc.unitmesh.untitled.demo.entity.Author;
 import lombok.Data;
 
 @Data
 public class CreateBlogRequest {
     private String title;
     private String content;
-    private User user;
+    private Author author;
 }
--- src/main/java/cc/unitmesh/untitled/demo/entity/Author.java
+++ src/main/java/cc/unitmesh/untitled/demo/entity/Author.java
@@ -6,17 +6,17 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
-public class User {
+public class Author {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     private String name;
 
-    public User() {
+    public Author() {
     }
 
-    public User(String name) {
+    public Author(String name) {
         this.name = name;
     }
 }
