2ddb4b4eaa08e5089a898f2f5f472b08a1cbec40
Phodal Huang <h@phodal.com>
2024-05-27 21:56:04 +0800

    test(blog): add unit tests for BlogService and BlogController
    Added unit tests for BlogService and BlogController. The tests cover the update and creation of blog posts. They also handle the case where a blog post is not found during an update operation.

--- src/test/java/cc/unitmesh/untitled/demo/service/BlogServiceTest.java
+++ src/test/java/cc/unitmesh/untitled/demo/service/BlogServiceTest.java
@@ -1,2 +1,56 @@
 package cc.unitmesh.untitled.demo.service;
 
+import cc.unitmesh.untitled.demo.entity.BlogPost;
+import cc.unitmesh.untitled.demo.repository.BlogRepository;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
+public class BlogServiceTest {
+
+    @MockBean
+    private BlogRepository blogRepository;
+
+    @Autowired
+    private BlogService blogService;
+
+    @Test
+    public void should_update_blog() {
+        // given
+        BlogPost existingBlog = new BlogPost("Existing Title", "Existing Content", "Existing Author");
+        existingBlog.setId(1L);
+        BlogPost newBlog = new BlogPost("New Title", "New Content", "New Author");
+        when(blogRepository.findById(1L)).thenReturn(Optional.of(existingBlog));
+        when(blogRepository.save(any(BlogPost.class))).thenAnswer(i -> i.getArguments()[0]);
+
+        // when
+        BlogPost updatedBlog = blogService.updateBlog(1L, newBlog);
+
+        // then
+        assertEquals("New Title", updatedBlog.getTitle());
+        assertEquals("New Content", updatedBlog.getContent());
+    }
+
+    @Test
+    public void should_return_null_when_blog_not_found() {
+        // given
+        BlogPost newBlog = new BlogPost("New Title", "New Content", "New Author");
+        when(blogRepository.findById(1L)).thenReturn(Optional.empty());
+
+        // when
+        BlogPost updatedBlog = blogService.updateBlog(1L, newBlog);
+
+        // then
+        assertNull(updatedBlog);
+    }
+}
