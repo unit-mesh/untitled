94108974eaaf4be82cef221a5ae19c1e9dbabbf7
Phodal Huang <h@phodal.com>
2025-02-10 17:48:58 +0800

    chore: update for template

--- src/main/java/cc/unitmesh/untitled/demo/entity/Author.java
+++ src/main/java/cc/unitmesh/untitled/demo/entity/Author.java
@@ -19,4 +19,20 @@ public class Author {
     public Author(String name) {
         this.name = name;
     }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
