a89133e1f32a8f1f24df10c80f5d188bcbd809fb
Phodal Huang <h@phodal.com>
2025-06-04 15:32:44 +0800

    feat: 为DemoApplication添加JPA审计功能
    - 在DemoApplication类上添加@EnableJpaAuditing注解
    - 导入org.springframework.data.jpa.repository.config.EnableJpaAuditing包

--- src/main/java/cc/unitmesh/untitled/demo/DemoApplication.java
+++ src/main/java/cc/unitmesh/untitled/demo/DemoApplication.java
@@ -1,9 +1,11 @@
 package cc.unitmesh.untitled.demo;
 
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class DemoApplication {
 
 	public static void main(String[] args) {
